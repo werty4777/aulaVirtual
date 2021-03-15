5de74fa4100dd310a5ae0c774b6f1ddb776e903c
Adrian Galindo <asdrian4455@hotmail.com>
2021-03-14 20:21:10 -0500

    auht

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app =express();
 app.use(express.static(__dirname + '/dist/AulaVirtual'))
 app.get('/*', function (req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/AulaVirtual/index.html'));
 });
 app.listen(process.env.PORT || 4200);
