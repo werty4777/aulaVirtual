3e21414561e9390f1763bde40e4f2b5bd7fff395
Adrian Galindo <asdrian4455@hotmail.com>
2021-03-14 20:19:30 -0500

    auht

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app =express();
-app.use(express.static(__dirname + '/dist/'))
+app.use(express.static(__dirname + '/dist/AulaVirtual'))
 app.get('/*', function (req,res){
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
